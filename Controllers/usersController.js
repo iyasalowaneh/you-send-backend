19d254381ddedffe99be58d002ca5c151a3d6a04
Abdullah <abdallah.b.b96@gmail.com>
2021-07-27 14:20:34 +0300

    name

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const twilio = require("twilio");
 exports.signup = async (req, res, next) => {
   try {
-    // req.body.phonenumber=
+ 
     let code = Math.floor(100000 + Math.random() * 900000);
     let client = new twilio(
       "AC2367b6cf83997dfed8e9eddafb4305ff",
@@ -20,12 +20,16 @@ exports.signup = async (req, res, next) => {
       })
       .then((message) => console.log(message.sid));
     const myUser = await User.findOne({
-      where: { phonenumber: req.body.phonenumber },
+      where:  { phonenumber: req.body.phonenumber } 
+
+      
     });
     if (myUser) await myUser.update({code})
     else
      await User.create({
       phonenumber: req.body.phonenumber,
+      name:req.body.name,
+      status:req.body.status,
       code,
     });
     // const token = generteToken(newUser);
@@ -44,11 +48,58 @@ exports.signin = async (req, res, next) => {
 const generteToken = (user) => {
   const payload = {
     id: user.id,
-    username: user.username,
-    firstName: user.firstName,
-    lastName: user.lastName,
+    name: user.name,
+   
     exp: Date.now() + parseInt(JWT_EXPIRATION_MS),
   };
   const token = jwt.sign(payload, JWT_SECRET);
   return token;
+};
+
+
+// exports.updateUser = async (req, res, next) => {
+//   console.log(req.User)
+//   try {
+//     // if (req.file) {
+//     //   req.body.image =` http://${req.get("host")}/media/${req.file.filename}`;
+//     // }
+//     await User.update(req.body);
+//     res.status(201).json(req.user);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    // if (req.file) {
+    //   req.body.image = http://${req.get("host")}/media/${req.file.filename};
+    // }
+    await req.user.update(req.body);
+    res.status(201).json(req.user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.fetchUser = async (userId, next) => {
+  try {
+    const user = await User.findByPk(userId);
+
+    return user;
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.printUsers = async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
 };
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const { session } = require("passport");
 const passport = require("passport");
-let { signup, signin } = require("../Controllers/usersController");
+let { signup, signin ,updateUser ,fetchUser ,printUsers} = require("../Controllers/usersController");
 
 const router = express.Router();
-
+router.param("userId", async (req, res, next, userId) => {
+  const user = await fetchUser(userId, next);
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    const err = new Error("user not found");
+    err.status = 404;
+    next(err);
+  }
+});
 router.post("/signup", signup);
 router.post(
   "/signin",
   passport.authenticate("local", { session: false }),
   signin
 );
+router.put(
+  "/:userId",  updateUser
+);
+
+router.get(
+  "/users",  printUsers
+);
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
